f57e02c5a5480318e2025b8efea0b35973ef871a
cesare <laurenziello@hotmail.it>
2019-04-18 19:42:41 +0200

    Add api key

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000;
 const webpush = require('web-push');
 
 const vapidKeys = {
-    "publicKey":"*",
-    "privateKey":"*"
+    "publicKey":"BGal3cKBZkSx8ymaFACUhGS1DpsgUO_xmjUzkk0zYr1es6pjFKscTQ_9RMv8zwOEYuedFWp_H91IAjuTKiapueM",
+    "privateKey":"GEDjdBnWIIMB_ImV0iYmCtTxWTyySsen6ig1Z5i3gyg"
 };
 
 
@@ -15,6 +15,6 @@ webpush.setVapidDetails(
     vapidKeys.privateKey
 );
 
-const server = app.listen(port, function() {
+app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
